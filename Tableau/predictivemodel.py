5b86b61fe3045d7cce18c437a1dca893bb84dd9c
scottbrwnng <72707817+scottbrwnng@users.noreply.github.com>
2022-04-25 08:36:13 -0400

    Update predictivemodel.py

--- Tableau/predictivemodel.py
+++ Tableau/predictivemodel.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression, LogisticRegressionCV
+from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
 df = pd.read_excel('/Users/scott/Desktop/Alchemy Project/Alchemy Broker Data(Cleaned, Null to Zero).xlsx', index_col='broker_name')
@@ -14,10 +15,13 @@ list1 = ['GWP - 2013', 'GWP - 2014', 'GWP - 2015', 'GWP - 2017', 'GWP - 2018', '
 
 df = df[list1]
 
+
 X_train, X_test, y_train, y_test = train_test_split(df) 
 
 reg = LinearRegression()
-reg.fit(df,df['GWP - 2019'])
+reg.fit(X_train, y_train)
+reg
+
 
 
 
